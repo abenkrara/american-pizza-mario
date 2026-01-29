439f06a8197c98dbf5aafe24c89ba4e1d2ed3d58
Ayub <abenkrara.7@gmail.com>
2026-01-29 21:56:49 +0100

    Feat: Add debug endpoint and diagnostic alert to Admin UI

---
+++ api/debug.js
@@ -0,0 +1,6 @@
+﻿export default function handler(req, res) {
+    res.status(200).json({
+        url_set: !!process.env.NEXT_PUBLIC_SUPABASE_URL,
+        key_set: !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || !!process.env.SUPABASE_SERVICE_ROLE_KEY
+    });
+}
